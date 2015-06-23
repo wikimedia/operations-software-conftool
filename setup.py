4913f07cbdee16fe4d1ba44a5cfd94c6b28a84ee
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2015-06-23 12:52:08 +0200

    debian: Version bump to 0.1.0
    Change-Id: I1070c2f6568ed6acedc1fa4a03c4c64aae28fc70

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='conftool',
-    version='0.0.3',
+    version='0.1.0',
     description='Collection of tools to interoperate with distributed k/v stores',
     author='Joe',
     author_email='glavagetto@wikimedia.org',
