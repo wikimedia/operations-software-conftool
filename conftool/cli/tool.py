bf5a994954b9f9a67a3469fd1d83e20198346ea3
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2018-02-20 12:11:49 +0100

    Fix parameters injection in ToolCliSimpleAction
    Change-Id: I2faffe564548446105fb47c4cd89f13806496627

--- conftool/cli/tool.py
+++ conftool/cli/tool.py
@@ -242,7 +242,7 @@ class ToolCliSimpleAction(ToolCliByLabel):
         args.selector = 'name={}'.format(socket.getfqdn())
         if 'service' in args and args.service is not None:
             args.selector += ',service={}'.format(args.service)
-        args.action = self.simple_actions[args.mode]
+        args.action = [self.simple_actions[args.mode]]
         args.mode = 'select'
         super(ToolCliSimpleAction, self).__init__(args)
 
