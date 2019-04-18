8e8c82587ce0fd8d4e96a450304d65f1ace31ce5
Chris Danis <cdanis@wikimedia.org>
2019-04-18 16:34:41 -0400

    conftool: remove 2.7 metadata, add 3.7
    Change-Id: I393d273e2d0510b35d721aa43a93126effd0862d

--- setup.py
+++ setup.py
@@ -25,11 +25,11 @@ setup(
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: System :: Clustering',
     ),
 )
