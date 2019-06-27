2676bd1885c00f40f952f219c604c80632f58154
Riccardo Coccioli <rcoccioli@wikimedia.org>
2019-06-27 18:59:46 +0200

    dbconfig: allow to remotely paste messages
    * Add a phaste() utility function to send text with a title to
      Phabricator's paste.
    
    Change-Id: Id04aa713bf208f8eec028a122d2808f8cc29bf55

--- conftool/extensions/dbconfig/action.py
+++ conftool/extensions/dbconfig/action.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+from subprocess import PIPE, Popen, TimeoutExpired
+
+
+PHASTE_EXECUTABLE = '/usr/local/bin/phaste'
+
+
 class ActionResult:
 
     def __init__(self, success, exit_code, *, messages=None, announce_message=''):
@@ -5,3 +12,22 @@ class ActionResult:
         self.exit_code = exit_code
         self.messages = messages if messages is not None else []
         self.announce_message = announce_message
+
+
+def phaste(title, message):
+    """Publish a message with a given title as a Phabricator paste and return its URL as string."""
+    if not Path(PHASTE_EXECUTABLE).exists():
+        return 'Skipping phaste: {path} not found'.format(path=PHASTE_EXECUTABLE)
+
+    try:
+        proc = Popen([PHASTE_EXECUTABLE, '--title', title], stdin=PIPE, stdout=PIPE,
+                     universal_newlines=True)
+        outs, errs = proc.communicate(input=message, timeout=5)
+        proc.terminate()
+    except TimeoutExpired:
+        proc.kill()
+        outs, errs = proc.communicate()
+        if outs is None or not outs:
+            outs = 'Unable to send diff to phaste'
+
+    return outs.strip()
