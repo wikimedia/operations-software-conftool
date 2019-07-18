249e746323d8c1964f10d1f5639b4c3f3daf32d3
Chris Danis <cdanis@wikimedia.org>
2019-07-18 14:34:17 -0400

    syncer: verify no data == no removal
    Verify that when conftool-sync runs without YAML files being given
    to define dbconfig's instances/sections, it won't touch the ones already
    present in etcd.
    
    Change-Id: I57456b47f07eb2ee9c9e70a0c291e32f166f0476

--- conftool/tests/integration/test_syncer.py
+++ conftool/tests/integration/test_syncer.py
@@ -1,8 +1,9 @@
 import os
 import contextlib
 from conftool.cli import syncer
-from conftool import service, node
-from conftool.tests.integration import IntegrationTestBase
+from conftool import loader, service, node
+from conftool.extensions.dbconfig.entities import Instance, Section
+from conftool.tests.integration import IntegrationTestBase, test_base
 import tempfile
 import yaml
 import shutil
@@ -112,3 +113,47 @@ class SyncerIntegration(IntegrationTestBase):
                 nodename = "node-%d" % i
                 n = node.Node(dc, cluster, servname, nodename)
                 self.assertFalse(n.exists)
+
+    def test_dbconfig_no_sync(self):
+        @contextlib.contextmanager
+        def temp_dbconfig_data(sections, instances=None, *, dc='testdc'):
+            directory = tempfile.mkdtemp()
+            sections_dir = os.path.join(directory, 'dbconfig-section')
+            instances_dir = os.path.join(directory, 'dbconfig-instance')
+            os.mkdir(sections_dir)
+            os.mkdir(instances_dir)
+            sections_file = os.path.join(sections_dir, 'test.yaml')
+            instances_file = os.path.join(instances_dir, 'test.yaml')
+            with open(sections_file, 'w') as fh:
+                yaml.dump({dc: sections}, fh)
+            if instances is not None:
+                with open(instances_file, 'w') as fh:
+                    yaml.dump({dc: instances}, fh)
+            yield directory
+            shutil.rmtree(directory)
+
+        # Create a dummy inst1 and section s1 and verify they were created in etcd.
+        dc = 'testdc'
+        with temp_dbconfig_data(['s1'], ['inst1'], dc=dc) as basepath:
+            sync = syncer.Syncer(os.path.join(test_base, 'fixtures', 'dbconfig', 'schema.yaml'),
+                basepath)
+            sync.load()
+
+        schema = loader.Schema.from_file(
+            os.path.join(test_base, 'fixtures', 'dbconfig', 'schema.yaml'))
+        instances = Instance(schema, lambda x: True)
+        sections = Section(schema, lambda x: True)
+        inst1 = instances.get('inst1', dc=dc)
+        sect1 = sections.get('s1', dc=dc)
+        self.assertIsNotNone(inst1)
+        self.assertIsNotNone(sect1)
+
+        # When the syncer runs without data, verify that existing instances and sections
+        # are not removed.
+        sync = syncer.Syncer(os.path.join(test_base, 'fixtures', 'dbconfig', 'schema.yaml'),
+            '/nonexistent')
+        sync.load()
+        inst1 = instances.get('inst1', dc=dc)
+        sect1 = sections.get('s1', dc=dc)
+        self.assertIsNotNone(inst1)
+        self.assertIsNotNone(sect1)
