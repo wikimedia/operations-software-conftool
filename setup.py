c25d30382209ef4f87c033b230114cf961822030
Chris Danis <cdanis@wikimedia.org>
2020-02-24 14:32:58 +0800

    drop support for Python 3.4
    Very few python3.4 hosts remaining in production, and for many of them,
    I don't believe pool/depool are meaningful/necessary.
    
    Also CI is currently broken because PyYAML no longer supports 3.4.
    
    Change-Id: Ic404709c1e421fd36ad903998b69e90e0847ff7b

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
