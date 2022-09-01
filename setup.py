421fdedf3cb5003d1e0a0bf2074d2bc8a1622b8f
Chris Danis <cdanis@wikimedia.org>
2022-09-01 06:53:49 -0400

    bump version
    Change-Id: I0b388cbee252caaa280800c50e492aba19fe1944

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="conftool",
-    version="2.2.1",
+    version="2.2.2",
     description="Tools to interoperate with distributed k/v stores",
     author="Joe",
     author_email="joe@wikimedia.org",
