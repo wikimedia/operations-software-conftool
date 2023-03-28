1cacda5fed79bb4c8573adea3ef06dbfccf2612c
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2023-03-28 10:27:18 +0200

    Release 2.3.0
    Change-Id: I75518a3abf6e04c0b4fc989cc6453091820c29e6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="conftool",
-    version="2.2.2",
+    version="2.3.0",
     description="Tools to interoperate with distributed k/v stores",
     author="Joe",
     author_email="joe@wikimedia.org",
