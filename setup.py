23dfb7f878e189790b21cdc7dfb1912edd60660d
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2024-06-19 07:19:12 +0200

    Release 3.0.1
    Bug: T367919
    Change-Id: I240f3f97bf475ef7fb6d79d6d3f39526e344ebed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="conftool",
-    version="3.0.0",
+    version="3.0.1",
     description="Tools to interoperate with distributed k/v stores",
     author="Joe",
     author_email="joe@wikimedia.org",
